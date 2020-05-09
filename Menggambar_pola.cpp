b69d0c6d2946c3a9c44a1f24df620eb4c6a494e8
rian.rainmaker <rian.rainmaker@gmail.com>
2020-05-09 19:26:51 +0700

    Menambahkan file cpp Menggambar pola

---
+++ Menggambar_pola.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Membuat function
+void gambar(){
+	int tinggi, panjang, x, y;
+	
+	cout<<"Masukkan tinggi gambar: ";
+	cin>>tinggi;
+	
+	panjang = tinggi *3;
+	
+	for (y = tinggi; y >= 1; y--){		  	 //Mengatur berapa banyak baris dibuat
+		for (x = 0; x < panjang; x++){		
+			if (x <= y-1 || x >= tinggi + y) //Mengatur karakter untuk menampilkan " (spasi)" dan "*"
+				cout<<" ";
+			else
+				cout<<"*";
+		}
+		cout<<"\n";
+	}
+}
+
+//Panggil function
+
+int main(){
+	gambar();
+}
